fc9dd02505036119783ccdf3ceda82ea35f65996
KudaJay <KudaJay@gmail.com>
2023-08-07 14:46:37 +0200

    Updated.v11

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -14,4 +14,12 @@ typddef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-size
+size_t print_listint(const listint_t *h);
+
+listint_t *add_nodeint(listint_t **head, const int n);
+
+void free_listint(listint_t *head);
+
+int check_cycle(listint_t *list);
+
+#endif /* LISTS_H */

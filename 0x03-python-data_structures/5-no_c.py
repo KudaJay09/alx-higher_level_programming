56f3517bf61b421e9bb4185d8eb1a4168684d171
KudaJay <KudaJay@gmail.com>
2023-08-14 12:16:12 +0200

    fixed 5-no_cp.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,14 +2,13 @@
 # removes all characters c and C from a string.
 
 def no_c(my_string):
-<<<<<<< HEAD
+
     updated_str = ''
     for i in my_string:
         if i != 'c'and i != 'C':
             updated_str += i
             return (updated_str)
-=======
+
     new_string = my_string.translate({ord(i): Non for i in 'cC'})
 
     return new_string
->>>>>>> 5f6dc0daac6fc7b2f191979753a1b3a623e5a055

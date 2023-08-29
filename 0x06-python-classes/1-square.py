b58821f39c307377dc91563c26cc58b796a046e3
KudaJay <KudaJay@gmail.com>
2023-08-30 00:30:15 +0200

    all tasks

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initialize new Square"""
+        self.__size = size

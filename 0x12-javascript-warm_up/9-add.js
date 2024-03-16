a5af73f71ca6e916ccfd52150512e96f14e1138b
KudaJay09 <micheal@gmail.com>
2024-03-16 00:16:35 -0700

    semistandard 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,6 @@
-function add(a, b) {
-    const c = a + b;
-    console.log(c);
+#!/usr/bin/node
+function add (a, b) {
+  return a + b;
 }
 
-add(Number(process.argv[2]), Number(process.argv[3]));
+console.log(add(Number(process.argv[2]), Number(process.argv[3])));

fd4a1f5c137bcc73154af18300fe34aae04aed0c
KudaJay09 <micheal@gmail.com>
2024-03-16 00:10:19 -0700

    7-mulit_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ const argv = process.argv;
 if (argv.length === 3 && !isNaN(argv[2])) {
   for (let i = 0; i < argv[2]; i++) {
     console.log('C is fun');
-}
+  }
 } else {
   console.log('Missing number of occurrences');
 }
